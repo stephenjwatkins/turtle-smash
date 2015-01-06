8e7c6903b951d392a7a8b215740f46ed28d79537
Stephen Watkins <stjowa@gmail.com>
2015-01-05 18:27:21 -0600

    Initial Commit

---
+++ Turtle SmashTests/Turtle_SmashTests.swift
@@ -0,0 +1,36 @@
+//
+//  Turtle_SmashTests.swift
+//  Turtle SmashTests
+//
+//  Created by Stephen Watkins on 1/5/15.
+//  Copyright (c) 2015 Stephen Watkins. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Turtle_SmashTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
